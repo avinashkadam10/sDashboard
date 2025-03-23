58a69f14c72936629b4c8e147c7eb65f063bb797
AvinashKadam10 <avinashanilkadam10@gmail.com>
2025-03-24 02:45:33 +0530

    ignore file

---
+++ src/components/Menu.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+
+import { Link } from "react-router-dom";
+
+const Menu = () => {
+  const [selectedMenu, setSelectedMenu] = useState(0);
+  const [isProfileDropdownOpen, setIsProfileDropdownOpen] = useState(false);
+
+  const handleMenuClick = (index) => {
+    setSelectedMenu(index);
+  };
+
+  const handleProfileClick = (index) => {
+    setIsProfileDropdownOpen(!isProfileDropdownOpen);
+  };
+
+  const menuClass = "menu";
+  const activeMenuClass = "menu selected";
+
+  return (
+    <div className="menu-container">
+      <img src="logo.png" style={{ width: "50px" }} />
+      <div className="menus">
+        <ul>
+          <li>
+            <Link
+              style={{ textDecoration: "none" }}
+              to="/"
+              onClick={() => handleMenuClick(0)}
+            >
+              <p className="setSelectedMenu === 0 ? activeMenuClass : menuClass">
+                Dashboards
+              </p>
+            </Link>
+          </li>
+          <li>
+            <Link
+              style={{ textDecoration: "none" }}
+              to="/orders"
+              onClick={() => handleMenuClick(1)}
+            >
+              <p className="setSelectedMenu===1 ? activeMenuClass : menuClass">
+                orders
+              </p>
+            </Link>
+          </li>
+          <li>
+            <Link
+              style={{ textDecoration: "none" }}
+              to="/holdings"
+              onClick={() => handleMenuClick(2)}
+            >
+              <p className="setSelectedMenu===2 ? activeMenuClass : menuClass">
+                Holdings
+              </p>
+            </Link>
+          </li>
+          <li>
+            <Link
+              style={{ textDecoration: "none" }}
+              to="/positions"
+              onClick={() => handleMenuClick(3)}
+            >
+              <p className="setSelectedMenu===3 ? activeMenuClass : menuClass">
+                Positions
+              </p>
+            </Link>
+          </li>
+          <li>
+            <Link
+              style={{ textDecoration: "none" }}
+              to="/funds"
+              onClick={() => handleMenuClick(4)}
+            >
+              <p className="setSelectedMenu===4 ? activeMenuClass : menuClass">
+                Funds
+              </p>
+            </Link>
+          </li>
+          <li>
+            <Link
+              style={{ textDecoration: "none" }}
+              to="/apps"
+              onClick={() => handleMenuClick(5)}
+            >
+              <p className="setSelectedMenu===5 ? activeMenuClass : menuClass">
+                Apps
+              </p>
+            </Link>
+          </li>
+        </ul>
+        <hr />
+        <div className="profile" onClick={handleProfileClick}>
+          <div className="avatar">ZU</div>
+          <p className="username">USERID</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;

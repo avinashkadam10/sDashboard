ee4b871b53a28208fabf7505190c08a6dee7cda0
AvinashKadam10 <avinashanilkadam10@gmail.com>
2025-03-24 03:05:48 +0530

    i

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import Home from "./components/Home";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <BrowserRouter basename="/">
+  <Routes>
+    <Route path="/*" element={<Home />} />
+  </Routes>
+</BrowserRouter>
   </React.StrictMode>
 );
